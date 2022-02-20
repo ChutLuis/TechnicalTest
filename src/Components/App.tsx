0384be5bbf465a6981ac4bfe25921d84191c28de
ChutLuis <l.chuta2014ig@gmail.com>
2022-02-19 21:41:38 -0600

    Design improvement

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,32 +2,44 @@ import React from "react";
 import "../Assets/CSS/App.css";
 import logo from "../Assets/Images/superheros-logo.png";
 // reactstrap components
-import { Button, Card, CardBody, Collapse, Row, Col } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  Collapse,
+  Row,
+  UncontrolledAccordion,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+} from "reactstrap";
 function App() {
   const [isOpen, setIsOpen] = React.useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+  const toggles = () => setIsOpen(!isOpen);
   return (
     <>
       <div className="Content">
-      <Row>
-        <Col g="3" md="6">
-        <Button
-              block
-              className="btn-sm mt-3"
-              color="info"
-              size="lg"
-              onClick={toggle}
-              style={{ marginBottom: "1rem" }}
-            >
-              Elegir Fecha
-            </Button>
-            <Collapse isOpen={isOpen}>
-              <div id="DatePickerDiv">
-                Pog               
-              </div>
-            </Collapse>
-        </Col>
-      </Row>
+        <Row>
+          <div className="imgHeader">
+            <img src={logo} alt="" />
+          </div>
+        </Row>
+        <Row>
+          <Button id="buttonLikes" onClick={toggles}>
+            Elegir Fecha
+          </Button>
+          <Collapse isOpen={isOpen}>
+            <Card>
+              <CardBody>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. Nihil anim keffiyeh
+                helvetica, craft beer labore wes anderson cred nesciunt sapiente
+                ea proident.
+              </CardBody>
+            </Card>
+          </Collapse>
+        </Row>
+        <Row></Row>
       </div>
     </>
   );
